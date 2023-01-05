d8f78aadc79ac62400f879a0d97b8270a18009ae
Abungo Thokchom <abungothokchom@gmail.com>
2023-01-05 16:19:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Hello There')
+    res.send('Hello There, Welcome to the Nodejs Webpage')
 })
 app.listen(process.env.PORT || 3000)
